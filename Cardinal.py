cb18cf711fc1ce08ac6bebc2016d68c1c4e8e6ce
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2023-02-13 11:03:06 -0500

    Update Cardinal.py

--- Cardinal.py
+++ Cardinal.py
@@ -1,5 +1,56 @@
-print()
+# Functions
 
-print("Hello World")
+def Login_F():
+    print()
+    
+def Account_Login_F():
+    print()
+    
+def Admin_Login_F():
+    print()
+
+def Account_F():
+    print()
+    
+def Change_User():
+    print()
+    
+def Change_Pass():
+    print()
+    
+def Admin_Account_F():
+    print()
+
+
+
+# Switching Variables
+
+Commandline = True
+Login = False
+Account_Login = False
+Account = False
+Admin_Login = False
+Admin_Account = False
+Admin_Confirm = False
+Admin_Confirm_User = False
+Admin_Confirm_Gen_User = False
+Admin_Confirm_Pass = False
+Admin_Confirm_Gen_Pass = False
+
+# Variables
+
+a_number = 0
+b_number = 0
+c_number = 0
+x_number = 0
+y_number = 0
+z_number = 0
+
+# Account Info
+
+Account_User = "Person"
+Account_Pass = "Password"
+Admin_User = "Admin"
+Admin_Pass = "Testing"
 
 print()
