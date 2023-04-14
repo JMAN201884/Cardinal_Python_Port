5c291f98f2581265bf2600a4070652a59948c1e8
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2023-04-14 11:30:52 -0400

    Update Test.bat

--- Dev/Temp/School/Test.bat
+++ Dev/Temp/School/Test.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\python\python.exe" "C:\Users\Jonathan113235\Documents\GitHub\Cardinal_Python_Port\Login.py"
+REM"C:\python\python.exe" "C:\Users\Jonathan113235\Documents\GitHub\Cardinal_Python_Port\Login.py"
 "C:\python\python.exe" "C:\Users\Jonathan113235\Documents\Cardinal_Python_Port\Cardinal.py"
 pause
