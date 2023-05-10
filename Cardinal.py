71af704b44a2b04bbb03a41cbab128f68a046e50
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2023-05-10 11:54:02 -0400

    Update Cardinal.py

--- Cardinal.py
+++ Cardinal.py
@@ -1,21 +1,46 @@
 # Functions
 
-print("1")
+
 
 # Switching Variables
 
-
+Commandline = True
+Login_V = False
+Account_Login_V = False
+Account_V = False
+Admin_Login_V = False
+Admin_Account_V = False
+Admin_Confirm = False
+Admin_Confirm_User = False
+Admin_Confirm_Gen_User = False
+Admin_Confirm_Pass = False
+Admin_Confirm_Gen_Pass = False
+Confirm = False
+Change_Gen_User_V = False
+Change_Gen_Pass_V = False
+Chagne_Admin_User_V = False
+Change_Admin_Pass_V = False
 
 # Variables
 
-
+cinnu = ""
+cinnp = ""
 
 # Registers
 
-
+a = 0
+b = 0
+c = 0
+x = 0
+y = 0
+z = 0
 
 # Account Info
 
+Account_User = "Guest"
+Account_Pass = "Password"
+Admin_User = "Admin"
+Admin_Pass = "Testing"
 
 
 # Body
