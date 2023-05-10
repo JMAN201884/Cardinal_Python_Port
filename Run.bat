53f82af676a310381b57e6497e3cc91242da88ae
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2023-05-10 11:48:45 -0400

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\python\python.exe" "C:\Users\Ichigo\Documents\GitHub\Cardinal_Python_Port\Cardinal.py"
+"C:\python\python.exe" "C:\Users\Jonathan113235\Documents\GitHub\Cardinal_Python_Port\Cardinal.py"
 pause
