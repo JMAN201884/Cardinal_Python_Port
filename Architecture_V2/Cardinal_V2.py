5a201a9a0c60ca95a8f1d4f6ce8048ade59e954d
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2023-11-02 09:10:40 -0400

    Create Cardinal_V2.py

---
+++ Architecture_V2/Cardinal_V2.py
@@ -0,0 +1,55 @@
+# Functions
+
+def Lgoin():
+    
+    ptint()
+
+def Main_User():
+
+    print()
+
+def Guest_User():
+
+    print()
+
+def Admin():
+
+    print()
+
+def Logout():
+
+    print()
+
+def Switch_Users():
+
+    print()
+
+def Exit():
+
+    print()
+
+# Switching Variables
+
+Commandline = True
+Login = False
+Account_Login = False
+Admin_Login = False
+Guest_Login = False
+Account_V = False
+Admin_v = False
+Guest_V = False
+
+
+# Variables
+
+
+
+# Registers
+
+
+
+# Account Info
+
+
+
+# Body
